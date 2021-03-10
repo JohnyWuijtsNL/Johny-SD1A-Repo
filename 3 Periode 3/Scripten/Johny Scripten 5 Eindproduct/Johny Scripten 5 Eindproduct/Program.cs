6675f8730e5832620d05a890c6c5e2f8416f2e3a
JohnyWuijtsSL <J.wuijts@sintlucasedu.nl>
2021-03-10 09:11:56 +0100

    Started on Johny Scripten 5 Eindproduct

---
+++ 3 Periode 3/Scripten/Johny Scripten 5 Eindproduct/Johny Scripten 5 Eindproduct/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Johny_Scripten_5_Eindproduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
