0bda605228917eb62569428f4d7d13d024d6e67d
JohnyWuijtsSL <J.wuijts@sintlucasedu.nl>
2021-03-11 14:40:11 +0100

    Added pong project

---
+++ 3 Periode 3/Scripten/Pong/Pong/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("██");
+            Console.WriteLine(" ▄▄");
+            Console.WriteLine(" ▀▀");
+            Console.WriteLine("▄▄ ");
+            Console.WriteLine("▀▀ ");
+        }
+    }
+}
