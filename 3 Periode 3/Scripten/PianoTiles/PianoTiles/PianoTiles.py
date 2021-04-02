053c042c199c5f403e569d3629854730cb05632f
JohnyWuijtsSL <J.wuijts@sintlucasedu.nl>
2021-04-02 12:14:33 +0200

    Changed Scripting binary files

--- 3 Periode 3/Scripten/PianoTiles/PianoTiles/PianoTiles.py
+++ 3 Periode 3/Scripten/PianoTiles/PianoTiles/PianoTiles.py
@@ -1,11 +1,11 @@
 #! python3
 import pyautogui, sys, keyboard
 while True:
-    if keyboard.is_pressed('v'):
+    if keyboard.is_pressed('Up'):
         pyautogui.click(730, 750)
-    elif keyboard.is_pressed('b'):
+    elif keyboard.is_pressed('Left'):
         pyautogui.click(880, 750)
-    elif keyboard.is_pressed('n'):
+    elif keyboard.is_pressed('Right'):
         pyautogui.click(1030, 750)
-    elif keyboard.is_pressed('m'):
+    elif keyboard.is_pressed('Down'):
         pyautogui.click(1180, 750)
