2b09fd6353bc6c9997770787df9657123014faab
JohnyWuijtsNL <J.wuijts@sintlucasedu.nl>
2021-09-09 11:47:43 +0200

    Added DwarfApp and CarApp

---
+++ 5 Periode 5/Programming/Johny Assignments SD week 1/CarApp/CarApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.fuel = 40;
+            car.Drive(240);
+            Console.WriteLine("Fuel left: " + car.fuel);
+            car.AddFuel(12);
+            Console.WriteLine("Fuel left: " + car.fuel);
+        }
+    }
+
+    class Car
+    {
+        public float fuel = 0;
+
+        public void Drive(float kilometers)
+        {
+            this.fuel -= kilometers * 0.05f;
+        }
+
+        public void AddFuel(float liters)
+        {
+            this.fuel += liters;
+        }
+    }
+}
---
+++ 5 Periode 5/Programming/Johny Assignments SD week 1/DwarfApp/DwarfApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DwarfApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dwarf dwalina = new Dwarf();
+            dwalina.name = "Dwalina";
+            Dwarf fili = new Dwarf();
+            fili.name = "Fili";
+
+            dwalina.SayHello();
+            fili.SayHello();
+        }
+    }
+
+    class Dwarf
+    {
+        public string name = "Dwarf";
+
+        public void SayHello()
+        {
+            Console.WriteLine("“Hello, my name is " + this.name + "!“");
+        }
+    }
+}
